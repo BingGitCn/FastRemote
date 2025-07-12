93fb61d4c721d5ee9aefc72ab301df4d850edd0e
Bing <mail.bing@qq.com>
2025-07-12 08:48:53 +0800

    添加项目文件。

---
+++ FastRemote/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+namespace  FastRemote.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _title = "Prism Application";
+      
+        public MainWindowViewModel()
+        {
+
+        }
+
+        [RelayCommand]
+        void Test()
+        { 
+        
+        }
+    }
+}
